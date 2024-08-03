505a73942444742112eab8c08dd9e55606a39c03
Muhammad Saad <saadstudent.cs@gmail.com>
2024-08-03 20:23:15 +0500

    Added: Controller with Full Functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
+import 'express-async-errors';
 
 import {
   errorHandler,
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,31 +2,13 @@ import { Router } from 'express';
 
 const router = Router();
 
-router
-  .route('/')
-  .get((req, res) => {
-    res.send(`
-        <section>
-            <h1>All Products</h1>
-            <a href='/api/v1/products/1'>Product 1</a>
-            <a href='/api/v1/products/2'>Product 2</a>
-        </section>
-        `);
-  })
-  .post((req, res) => {
-    res.send('Create new product');
-  });
+import {
+  getAllProducts,
+  getAllProductsStatic,
+} from '../controllers/productControllers.js';
 
-router
-  .route('/:id')
-  .get((req, res) => {
-    res.send(`Product ${req.params.id}`);
-  })
-  .put((req, res) => {
-    res.send(`Update product ${req.params.id}`);
-  })
-  .delete((req, res) => {
-    res.send(`Delete product ${req.params.id}`);
-  });
+router.route('/').get(getAllProducts);
+
+router.route('/static').get(getAllProductsStatic);
 
 export default router;
